00ee1cf564fa8e1953a228f073fdd153e1754a97
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2023-07-04 15:21:08 +0200

    Providers

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,22 @@
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { User } from "../../entities/User";
+
 export class CreateUserUseCase {
-    async execute(){
+    constructor(
+        private usersRespository: IUsersRepository
+    ){}
+
+    async execute(data: ICreateUserRequestDTO){
+        const userAlreadyExists = await this.usersRespository.findByEmail(data.email);
+
+        if(userAlreadyExists){
+            throw new Error('User already exists.')
+        }
+
+        const user = new User(data)
+
+        await this.usersRespository.save(user);
         
     }
 }
